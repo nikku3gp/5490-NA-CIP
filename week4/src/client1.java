18aa48863b75569d45a5a61e2815171b4a6640e6
Nikola <nikola.at@hotmail.com>
2025-03-07 19:23:48 +0100

    no access to the class repository :)

---
+++ week4/src/client1.java
@@ -0,0 +1,5 @@
+public class client1 {
+    public static void main(String[] args) {
+        //Don't have access to the class repository in order to proceed with exercises.
+    }
+}
--- week4/src/exc1.java
+++
@@ -1,5 +0,0 @@
-public class exc1 {
-    public static void main(String[] args) {
-
-    }
-}
