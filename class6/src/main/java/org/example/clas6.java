fb00f4b7f264f0712de109295e9f66cf702b0a80
Nikola <nikola.at@hotmail.com>
2025-04-04 19:29:16 +0200

    class6

--- class6/src/main/java/org/example/clas6.java
+++ class6/src/main/java/org/example/clas6.java
@@ -6,18 +6,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
+import java.util.Map;
 
 public class clas6 {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
 
-        Student student = new Student("A", 2, "null", List.of("Math", "Science"));
+        Student student = new Student("A", 2, "null", Map.of("Math", 5.2, "Science", 6.2));
 
         String json = mapper.writeValueAsString(student);
         System.out.println(json);
 
         Student student2 = mapper.readValue(json, Student.class);
-        System.out.println("Student: name: " +student2.getName());
+        System.out.println("Student name: " +student2.getName());
 
     }
     static class Student{
@@ -26,24 +27,23 @@ public class clas6 {
         @JsonProperty("email_address")
         String email;
 
-        @JsonIgnore
-        List<String> courses;
+        Map<String, Double> grades;
 
         public Student() {}
 
-        public Student(String name, int age, String email, List<String> courses){
+        public Student(String name, int age, String email, Map<String, Double> grades){
             this.name = name;
             this.age = age;
             this.email = email;
-            this.courses = courses;
+            this.grades = grades;
         }
 
-        public List<String> getCourses() {
-            return courses;
+        public Map<String, Double> getGrades() {
+            return grades;
         }
 
-        public void setCourses(List<String> courses) {
-            this.courses = courses;
+        public void setGrades(Map<String, Double> grades) {
+            this.grades = grades;
         }
 
         public String getName() {
