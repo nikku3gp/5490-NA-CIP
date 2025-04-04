a352215c6d7740c4ad19c8ac0b59d4bddb3cd9fb
Nikola <nikola.at@hotmail.com>
2025-04-04 19:18:31 +0200

    class6

--- class6/src/main/java/org/example/clas6.java
+++ class6/src/main/java/org/example/clas6.java
@@ -1,24 +1,49 @@
 package org.example;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
 public class clas6 {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
 
-        Student student = new Student("A", 2, "B");
+        Student student = new Student("A", 2, "null", List.of("Math", "Science"));
 
         String json = mapper.writeValueAsString(student);
         System.out.println(json);
+
+        Student student2 = mapper.readValue(json, Student.class);
+        System.out.println("Student: name: " +student2.getName());
+
     }
     static class Student{
         String name;
         int age;
+        @JsonProperty("email_address")
         String email;
 
-        public Student(String name, int age, String email){
+        @JsonIgnore
+        List<String> courses;
+
+        public Student() {}
+
+        public Student(String name, int age, String email, List<String> courses){
             this.name = name;
             this.age = age;
             this.email = email;
+            this.courses = courses;
+        }
+
+        public List<String> getCourses() {
+            return courses;
+        }
+
+        public void setCourses(List<String> courses) {
+            this.courses = courses;
         }
 
         public String getName() {
@@ -36,7 +61,7 @@ public class clas6 {
         public void setAge(int age) {
             this.age = age;
         }
-
+         @JsonInclude(JsonInclude.Include.NON_NULL)
         public String getEmail() {
             return email;
         }
