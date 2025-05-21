63f0191e62f75a7fe96ab975e5fa1200d93f798f
atadaweeb <nikola.at@hotmail.com>
2025-05-21 05:50:27 +0200

    init

---
+++ supplement/src/main/java/uacs/cip/nutrient/supplement/SupplementApplication.java
@@ -0,0 +1,13 @@
+package uacs.cip.nutrient.supplement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementApplication.class, args);
+	}
+
+}
