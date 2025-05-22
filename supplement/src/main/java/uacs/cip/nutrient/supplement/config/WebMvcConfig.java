20508e9ab0feb8c0bfd963382f6192946c5b1ff8
atadaweeb <nikola.at@hotmail.com>
2025-05-22 03:44:15 +0200

    WebMvcConfig

---
+++ supplement/src/main/java/uacs/cip/nutrient/supplement/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package uacs.cip.nutrient.supplement.config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+    private static final Logger logger = LoggerFactory.getLogger(WebMvcConfig.class);
+    private final ApiKeyInterceptor apiKeyInterceptor;
+
+    public WebMvcConfig(ApiKeyInterceptor apiKeyInterceptor){
+        this.apiKeyInterceptor = apiKeyInterceptor;
+        logger.info("API key interceptor configured for all controller endpoints");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+
+    }
+}
